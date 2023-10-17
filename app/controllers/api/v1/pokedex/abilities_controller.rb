6b1a15ebce06e849c06a5d9ebdaf0ee5e948c67e
vantung2102 <vantung2102@gmail.com>
2023-10-17 22:42:52 +0700

    code

--- app/controllers/api/v1/pokedex/abilities_controller.rb
+++ app/controllers/api/v1/pokedex/abilities_controller.rb
@@ -1,8 +1,20 @@
 module API
   module V1
-    module Pokemon
+    module Pokedex
       class AbilitiesController < BaseController
+        include Resourceable
 
+        skip_before_action :authenticate_user!
+
+        resource_with class: Ability,
+                      collection_serializer: ::Pokedex::AbilitySerializer,
+                      object_serializer: ::Pokedex::AbilitySerializer
+
+        private
+
+        def resource_permitted_params
+          params.require(:ability).permit(::API::Pokedex::AbilityPolicy.permitted_attributes)
+        end
       end
     end
   end
